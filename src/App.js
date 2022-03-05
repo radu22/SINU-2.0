01d79b9d9477f948d9ff096f880ae17960fcf89f
radu22 <catarama.radu22@gmail.com>
2022-03-05 12:49:14 +0200

    PR testing

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. muie
-        </p>
-        <h1> edit adriana</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactieaaaaa
-        </a>
+        <h1>Fresh start!</h1>
       </header>
     </div>
   );
