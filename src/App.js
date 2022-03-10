346d667d561ee383864560266c5e02839d44afe7
Daymio1 <alexandru-razvanmuresan@outlook.com>
2022-03-10 12:27:23 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Fresh start!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>Fresh start !</h1>
+                <h2>Fresh start !</h2>
+            </header>
+        </div>
+    );
 }
 
 export default App;
